7d55e3cc41510d784c0166326ca2034449f95a72
Arkadiusz Bauer <arek.bauer12@gmail.com>
2024-07-12 01:29:39 +0100

    Add: Started Pokemon Go Events Script

---
+++ PokemonGo.py
@@ -0,0 +1,10 @@
+"""
+By using https://github.com/bigfoott/ScrapedDuck
+
+I want to grab events and only put in the start of it, e.g.
+
+- Start of when a new raid boss comes in 
+- Full events
+- Changing descriptions when event is known
+- Hosting all of this on the raspberry pi
+"""
